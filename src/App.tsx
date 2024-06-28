02afc778e9e3b2bc8b7fe1d19640ab28f7264447
jaqwe2301 <jaqwe2301@naver.com>
2024-06-28 17:24:34 +0900

    Chore: 필요없는 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useReactiveVar } from "@apollo/client";
 import { LoggedInRouter } from "./routers/logged-in-router";
 import { LoggedOutRouter } from "./routers/logged-out-router";
 import { isLoggedInVar } from "./apollo";
-import { useEffect } from "react";
 
 export const App = () => {
   // useEffect(() => {
